9caef3f9d5101d9933bff00778896b50bf5260e0
shravan-das <100745475+shravan-das@users.noreply.github.com>
2022-03-05 19:56:51 +0530

    Add files via upload

---
+++ LAB1.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int search(int arr[], int n , int key){
+    for(int i = 0 ; i<n; i++){
+        if(arr[i]==key){
+        return i;
+        }
+        else{
+            cout<<"invalid"<<endl;
+        }
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i = 0 ; i<n ; i++){
+        cin>>arr[i];
+    }
+    int key;
+    int index;
+    cout<<"enter the key to searched:"<<endl;
+    cin>>key;
+    index =  search(arr,n,key);
+}
